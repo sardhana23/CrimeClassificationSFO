63719df7c58b8ecd1e6370965bb12334deb8d767
Somasundaram Ardhanareeswaran <sxa146230@utdallas.edu>
2016-04-13 23:08:00 -0500

    DateTime conversion to date, month, year and hour

---
+++ dataMassaging.R
@@ -0,0 +1,11 @@
+library('chron')
+data <- read.csv("Dataset/train.csv",header = T,sep = ",")
+
+data$Dates <- as.character(data$Dates)
+dtparts = t(as.data.frame(strsplit(data$Dates,' ')))
+thetimes = chron(dates=dtparts[,1],times=dtparts[,2],format=c('y-m-d','h:m:s'))
+
+data$Year <- format(thetimes, format="%y")
+data$Month <- format(thetimes, format="%m")
+data$Day <- format(thetimes, format="%d")
+data$hour <- format(thetimes, format="%H")
