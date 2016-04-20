6789261737b258b6113928c22e75040483af6102
Ubuntu <ubuntu@ip-172-31-63-10.ec2.internal>
2016-04-20 19:20:51 +0000

    Changes

--- Project.R
+++ Project.R
@@ -7,6 +7,7 @@ library(ipred)
 library(maboost)
 library(class)
 library('chron')
+library(adabag)
 
 #Read Data
 data <- read.csv("Dataset/train.csv",header = T,sep = ",")
@@ -17,6 +18,8 @@ category_data <- data.frame(table(data$Category))
 category_ordered <- category_data[order(category_data$Freq,decreasing = T),]
 category_needed <- head(category_ordered$Var1,n=22)
 data_new <- data[which(data$Category %in% category_needed),]
+data_new$Category <- factor(data_new$Category)
+
 
 #Format Address Column
 data_new$Address <- mapply(gsub, "^.*?Block of ","", data_new["Address"])
@@ -26,10 +29,26 @@ data_new$Dates <- as.character(data_new$Dates)
 dtparts = t(as.data.frame(strsplit(data_new$Dates,' ')))
 thetimes = chron(dates=dtparts[,1],times=dtparts[,2],format=c('y-m-d','h:m:s'))
 
-data_new$Year <- format(thetimes, format="%y")
+data_new$Year <- format(thetimes, format="%Y")
 data_new$Month <- format(thetimes, format="%m")
 data_new$Date <- format(thetimes, format="%d")
 data_new$hour <- format(thetimes, format="%H")
+
+data_new$Year <- as.factor(data_new$Year)
+data_new$Month <- as.factor(data_new$Month)
+data_new$Date <- as.factor(data_new$Date)
+data_new$hour <- as.factor(data_new$hour)
+
+data_formatted <- data_new[,c("Category","DayOfWeek","PdDistrict","Address","X","Y","Year","Month","Date","hour")]
+
+sample_data<-sample(1:nrow(data_formatted),size = 0.8*nrow(data_formatted))
+training_data_set<-data[sample_data,]
+test_data_set<-data[-sample_data,]
+
+form <- as.formula("Category ~ DayOfWeek+PdDistrict+X+Y+Year+Month+Date+hour")
+bag_model <- bagging(form, training_data_set, mfinal = 100)
+
+
 
 
 #data <- read.csv("C:/Users/Biligiri Vasan/Desktop/Spring 2016/ML/train.csv",header = T,sep = ",",na.strings = c("?"))
